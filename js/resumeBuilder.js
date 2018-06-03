360fc65c82f89302d554a1272244b65510149be8
Adriano Ribeiro <adriano@atmosferapublicidade.com.br>
2018-06-03 16:13:41 -0300

    update CV

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,6 +3,83 @@
 
  var formattedRole = HTMLheaderRole.replace("%data%", role);
 
+ var skills = ["awesomeness", "programming", "CSS3","JS","Semantic", "Design Responsive"]
 
- $("#header").prepend(formattedRole);
- $("#header").prepend(formattedName);
+//  var bio = {
+//      "name" : "Adriano Ribeiro",
+//      "role" : "Desenvolvedor",
+//      "contacts" : {
+//          "mobile" : "11 98736-0129",
+//          "email" : "adrianofrontend@gmail.com",
+//          "github" : "adrianofrontend",
+//          "location" : "Brazil"
+//      },
+
+//      "welcomeMesage" : "Aprendendo JS",
+//      "skills" : ["awesomeness", "programming", "CSS3","JS","Semantic", "Design Responsive"],
+//      "bioPic" : "https://adribeiro.com.br/wp-content/uploads/2015/11/banner_programador_preto_2.jpg"
+     
+//  };
+
+ var education = {
+     "schools": [
+         {
+             "name" : "Eckerd College",
+             "city" : "Saint Petersburg, FL, US",
+             "degree" : "BA",
+             "majors" : ["CS"],
+             "dates" : 2018,
+             "url" : "http://example.com"
+         },
+
+         {
+             "name" : "Nova Southeastern University",
+             "city" : "Fort Lauderdale, FL, US",
+             "degree" : "Masters",
+             "majors" : ["CS"],
+             "dates" : 2018,
+             "url" : "http://example.com"
+         }
+     ],
+
+     "onlineCourses" : [
+         {
+             "title": "JavaScript",
+             "school" : "Udacity",
+             "dates" : 2018
+         }
+     ]
+ }
+
+ var work = {
+     "jobs" : [
+         {
+             "employer": "Tesla Tecnologia",
+             "title": "Programming HTML JR",
+             "dates" : 2014,
+             "description" : "Development Landing Pages, Emails, WebSites"
+         },
+
+         {
+            "employer": "Soul Digital",
+            "title": "Programming Web",
+            "dates" : 2015,
+            "description" : "Development Landing Pages, Emails, WebSites"
+         }
+
+     ]
+ }
+
+
+
+//  var work = {};
+//  work.position = "Course Developer",
+//  work.employer = "Udacity",
+//  work.years = 0.3,
+ 
+
+//  $("#header").prepend(formattedRole);
+//  $("#header").prepend(formattedName);
+
+
+//  $("#main").append(work.position);
